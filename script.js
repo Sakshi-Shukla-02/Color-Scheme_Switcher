277bc0e35943d6a811e21e30c122b52b82e71e8f
Sakshi-Shukla-02 <sakshishukla271102@gmail.com>
2024-12-30 17:27:59 +0530

    Files Uploaded

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const redButton = document.getElementById("red").addEventListener("click" , () => {
+    document.body.style.backgroundColor = "rgb(177, 51, 51)";
+})
+
+
+const greenBtn  = document.getElementById("green").addEventListener("click" , () => {
+    document.body.style.backgroundColor = " rgb(57, 132, 57)";
+})
+
+const purpleBtn = document.getElementById("purple").addEventListener("click" , () => {
+    document.body.style.backgroundColor = "rgb(134, 74, 134)";
+})
+
+const whiteBtn =  document.getElementById("white").addEventListener("click" , () => {
+    document.body.style.backgroundColor = "white";
+})
